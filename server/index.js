75ba95fb916cb2b8ff71f304a3e0de0143c2a978
Ephraim Becker <emb@paay.co>
2022-12-22 11:42:32 -0500

    Updated directory

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 app.use(cors());
 app.use(express.json());
 
+app.post("/todos", async (req, res) => {
+    try {
+        res.json(req.body);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 app.listen(5000, () => {
     console.log("Server is starting on port 5000");
