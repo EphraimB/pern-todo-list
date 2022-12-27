7ffdff68052d9fd0f3fb0d6c7f968c6b7e211464
Ephraim Becker <emb16@outlook.com>
2022-12-27 15:07:50 -0500

    You can now add a todo.

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -4,14 +4,33 @@ import Form from 'react-bootstrap/Form';
 
 const InputTodo = () => {
   const [description, setDescription] = useState("");
-  console.log(description);
   
+  const onSubmitForm = async e => {
+    e.preventDefault();
+
+    try {
+      const body = { description };
+
+      const response = await fetch("http://localhost:5000/todos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+      });
+
+      console.log(response);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   return (
     <Fragment>
       <h1 className="text-center my-5">Input Todo</h1>
-      <Form className="d-flex">
+      <Form className="d-flex" onSubmit={onSubmitForm}>
         <Form.Control type="text" placeholder="add todo" value={description} onChange={e => setDescription(e.target.value)} />
-        <Button variant="success">Add</Button>
+        <Button type="submit" variant="success">Add</Button>
       </Form>
     </Fragment>
   )
