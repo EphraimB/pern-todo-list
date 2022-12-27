7974ecdb1babc6cb28efbd3189b8efda12da9f9f
Ephraim Becker <emb16@outlook.com>
2022-12-27 18:18:17 -0500

    Added a container.

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Container>
+      <App />
+    </Container>
   </React.StrictMode>
 );
