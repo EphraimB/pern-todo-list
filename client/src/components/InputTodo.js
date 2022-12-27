c7899bb8c35a386a571d8abf8a68f9e9f77a80a5
Ephraim Becker <emb16@outlook.com>
2022-12-27 12:13:54 -0500

    onChange event on input.

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -3,11 +3,14 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 const InputTodo = () => {
+  const [description, setDescription] = useState("");
+  console.log(description);
+  
   return (
     <Fragment>
       <h1 className="text-center my-5">Input Todo</h1>
       <Form className="d-flex">
-        <Form.Control type="text" placeholder="add todo" />
+        <Form.Control type="text" placeholder="add todo" value={description} onChange={e => setDescription(e.target.value)} />
         <Button variant="success">Add</Button>
       </Form>
     </Fragment>
